3e35239d649e506f12cfc24a618546131ddc20c8
EPeiffer94 <E.Peiffer@live.com>
2026-07-20 10:06:16 -0400

    marmot 2.8.2

--- Casks/marmot.rb
+++ Casks/marmot.rb
@@ -2,8 +2,8 @@
 # Lives in the tap repo: github.com/EPeiffer94/homebrew-marmot (Casks/marmot.rb)
 # Regenerate for each release with: sh scripts/make-cask.sh
 cask "marmot" do
-  version "2.8.1"
-  sha256 "0d960be431026841f2c8f86965ee6bf00e327de073f75207a1c23af1d1e64314"
+  version "2.8.2"
+  sha256 "b4f3d5e12b1d213cb3b83821d2bc1ad40f10d57795a22ed28910c935e74b507f"
 
   url "https://github.com/EPeiffer94/Marmot/releases/download/v#{version}/Marmot-#{version}.zip"
   name "Marmot"
