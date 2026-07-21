a769c33629e0b795f9af421a4adcd88bd893e9b2
EPeiffer94 <E.Peiffer@live.com>
2026-07-21 11:04:44 -0400

    marmot 3.0.4

--- Casks/marmot.rb
+++ Casks/marmot.rb
@@ -2,8 +2,8 @@
 # Lives in the tap repo: github.com/EPeiffer94/homebrew-marmot (Casks/marmot.rb)
 # Regenerate for each release with: sh scripts/make-cask.sh
 cask "marmot" do
-  version "3.0.3"
-  sha256 "d4e76520bdcd33544dc7cd0bb05ae342893e14541280504931120e51a8607601"
+  version "3.0.4"
+  sha256 "be7bab68264bd3f73a7a22531d7d06bc596193c93806aa60e3299220bb7ec187"
 
   url "https://github.com/EPeiffer94/Marmot/releases/download/v#{version}/Marmot-#{version}.zip"
   name "Marmot"
